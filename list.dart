323648b82048a9ec4e25734e17f1b2eacf8c12a0
akbar003 <khanakbar0233@gmail.com>
2023-09-21 14:15:18 +0500

    list in dart

---
+++ list.dart
@@ -0,0 +1,10 @@
+void main() {
+  List exe = ["akbar", "arif", "khan"];
+  print(exe.length);
+  print(exe[0]);
+  exe[1] = "usama";
+  print(exe);
+  int listlength = exe.length;
+  print(listlength);
+  print(exe[exe.length - 2]);
+}
